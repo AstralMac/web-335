2de2ddcad24c6ab0192d41900cae98325c7cfdf0
Malcolm Abdullah <mabdullah4562@gmail.com>
2024-08-26 21:55:00 -0500

    update

---
+++ Python/week_2/newprogram.py
@@ -0,0 +1,15 @@
+"""
+Author: Malcolm Abdullah
+Date: August 25th, 2024
+File Name: newprogram.py
+Description: This program prints a quote by Albert Einstein and gives credit to the author.
+"""
+
+# The quote to be printed
+quote = "Life is like riding a bicycle. To keep your balance, you must keep moving."
+
+# The author of the quote
+author = "Albert Einstein"
+
+# Printing the quote along with the author's name
+print(f'"{quote}" - {author}')
